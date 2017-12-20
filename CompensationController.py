3c761d7861761775a3b90c4990599cd42d1ff250
ngoldfarb <nagoldfarb@wpi.edu>
2017-12-20 11:21:50 -0500

    controller to do gravity comp and read torque for joint 1

---
+++ CompensationController.py
@@ -0,0 +1,37 @@
+
+from Dynamics import Dynamics
+from Main.Robot import Robot
+import copy
+import numpy as np
+
+
+class CompensationController():
+    def __init__(self, k_l,k_v):
+        self._prev = None
+        self._K_l = k_l
+        self._K_v = k_v
+        pass
+
+    def getTorque(self, robot):
+
+        self._prev  = copy.deepcopy(robot)
+        g = Dynamics.make_gravity_matrix(robot)
+        M = Dynamics.make_mass_matrix(robot)
+        q = np.asarray(robot.q).reshape(3, 1)
+        qd = np.asarray(robot.qd).reshape(3, 1)
+        qdd = np.asarray(robot.qdd).reshape(3, 1)
+        load = np.asarray(robot.tau).reshape(3, 1)
+
+        u =  -( self._K_v*qd) + (self._K_l*load)   #
+        u[1] = g[1]
+        u[2] = g[2]
+        return u
+
+    def update_K_l(self, k):
+        self._K_l = k
+
+    def update_K_v(self, k):
+        self._K_v = k
+
+
+
