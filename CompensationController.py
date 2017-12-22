1129971bde4cdc77db0310a5345b1e5cafc494d1
ngoldfarb <nagoldfarb@wpi.edu>
2017-12-22 18:16:35 -0500

    added docstrings and graviy gain field

--- CompensationController.py
+++ CompensationController.py
@@ -6,25 +6,34 @@ import numpy as np
 
 
 class CompensationController():
-    def __init__(self, k_l,k_v):
+    def __init__(self, k_l, k_v, k_g):
+        """
+
+        :param k_l:
+        :param k_v:
+        :param k_g:
+        """
         self._prev = None
         self._K_l = k_l
         self._K_v = k_v
+        self._K_g = k_g
         pass
 
     def getTorque(self, robot):
+        """
 
+        :param robot:
+        :return:
+        """
         self._prev  = copy.deepcopy(robot)
         g = Dynamics.make_gravity_matrix(robot)
         M = Dynamics.make_mass_matrix(robot)
         q = np.asarray(robot.q).reshape(3, 1)
         qd = np.asarray(robot.qd).reshape(3, 1)
-        qdd = np.asarray(robot.qdd).reshape(3, 1)
         load = np.asarray(robot.tau).reshape(3, 1)
 
-        u =  ( self._K_v*qd) + (self._K_l*load)   #
-        u[1] = 0.05*g[1]
-        u[2] = 0.005*g[2]
+        u = self._K_g*g +  ( self._K_v*qd) + (self._K_l*load)   #
+
         return u
 
     def update_K_l(self, k):
@@ -32,6 +41,9 @@ class CompensationController():
 
     def update_K_v(self, k):
         self._K_v = k
+
+    def update_K_g(self, k):
+        self._K_g = k
 
 
 
